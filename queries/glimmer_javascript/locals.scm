5bcdf6d9aeca81e1ca09589e1ae8678111621e44
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-08-20 17:13:17 -0400

    Try extending like this

--- queries/glimmer_javascript/locals.scm
+++ queries/glimmer_javascript/locals.scm
@@ -1,20 +1,2 @@
-; Scopes
-;-------
-[
-  (statement_block)
-  (function_expression)
-  (arrow_function)
-  (function_declaration)
-  (method_definition)
-] @local.scope
-
-; Definitions
-;------------
-(pattern/identifier) @local.definition
-
-(variable_declarator
-  name: (identifier) @local.definition)
-
-; References
-;------------
-(identifier) @local.reference
+;; inherits: javascript
+;; extends
