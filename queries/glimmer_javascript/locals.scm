3c47aa39e7af834e33e9dd34f06703c477cba45f
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2024-08-20 17:07:17 -0400

    Can't inherit, because of JSX

--- queries/glimmer_javascript/locals.scm
+++ queries/glimmer_javascript/locals.scm
@@ -1,2 +1,20 @@
-;; inherits: javascript
-;; extends
+; Scopes
+;-------
+[
+  (statement_block)
+  (function_expression)
+  (arrow_function)
+  (function_declaration)
+  (method_definition)
+] @local.scope
+
+; Definitions
+;------------
+(pattern/identifier) @local.definition
+
+(variable_declarator
+  name: (identifier) @local.definition)
+
+; References
+;------------
+(identifier) @local.reference
